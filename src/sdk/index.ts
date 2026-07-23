8199e9b2a60d5e57c7cbb8cdc1c1d15d4e5082f1
kaushalbhat <kaushalbhat0@gmail.com>
2026-07-24 04:40:38 +0530

    EPIC-00: remove src/sdk/ (4 files, verified orphaned)

--- src/sdk/index.ts
+++
@@ -1,3 +0,0 @@
-export type { ArtifactType, GeneratorContext, GeneratedFile, GeneratorResult, Generator, TemplateFile, TemplateDefinition, TemplateRegistryEntry } from "./types";
-export { TemplateEngine, GeneratorRegistry, TemplateRegistry, generatorRegistry, templateRegistry } from "./engine";
-export { moduleGenerator, themeGenerator, capabilityGenerator, providerGenerator, pluginGenerator, domainGenerator, surfaceGenerator, registerAllGenerators } from "./generators";
